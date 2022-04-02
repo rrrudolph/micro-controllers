a3cd1ad2afed3cd3565c9cabd9c47ff5369a84c0
Rudy Selman <rudysemail@gmail.com>
2022-04-02 16:31:40 -0500

    finished I think

--- PEMF/micropython-pemf.py
+++ PEMF/micropython-pemf.py
@@ -1,57 +1,71 @@
 from machine import Pin
-import utime
+import time
 import random
 
-# built in duty cycle values are 0 to 65025 so
-# need a conversion function to map 100% to 65025
-def get_duty_cycle(percentage):
-    # pass in a decimal, like 0.20 for 20%
-    return percentage * 65025
 
-# for each whole number frequency step, I want to choose
-# 3 random decimal values within that number to add
-# random variety to the frequencies that are generated
-def get_random_decimals(integer, order=None):
-    # I guess for micropython, random.seed() gives you a way
-    # to provide deterministic randomness but is not necessary
-    # for random randomness
-    if order == 'descend':
-        # order by descending values
-        a = random.uniform(0.66, 0.99)
-        b = random.uniform(0.33, 0.66)
-        c = random.uniform(0, 0.33)
-    
-    else:
-        # order by ascending
-        a = random.uniform(0, 0.33)
-        b = random.uniform(0.33, 0.66)
-        c = random.uniform(0.66, 0.99)
-    
-    return a, b, c
+# This code is meant specifically for the RP2040 chip
+# which is found in the RPi Pico. Can't confirm if this
+# code would run on other micropython boards
 
-def generate_frequencies(start, stop, minutes):
+
+DUTY_CYCLE = 0.15  # 15%
+SIGNAL = Pin(14, Pin.OUT) 
+
+
+# 
+def _get_random_values(frequency: int, order='descend'):
+    """Append random decimal values to each whole number for variety"""
+
+    random.seed()
+    a = frequency + random.uniform(0.75, 0.99)
+    b = frequency + random.uniform(0.5, 0.75)
+    c = frequency + random.uniform(0.25, 0.5)
+    d = frequency + random.uniform(0, 0.25)
+
+    return a, b, c, d if order == 'descend' else d, c, b, a
+
     
-    total_time = minutes * 60  # convert to seconds
+def _generate_frequency(frequency: float, duration: int):
+    """Duration needs to be in milliseconds"""
+    
+    # convert frequency from hz to milliseconds
+    freq = frequency / 1000
+    
+    # get on / off times
+    signal_on = freq * DUTY_CYCLE * 65025 # built in max value
+    signal_off = freq - signal_on
+    
+    end = time.ticks_ms() + duration
+    
+    while time.ticks_ms() < end:
+        SIGNAL.on()
+        time.sleep_ms(signal_on)
+        SIGNAL.off()
+        time.sleep_ms(signal_off)
+        
+    
+def run_frequencies(start, stop, minutes):
+    
+    total_milliseconds = minutes * 60 * 1000
     total_frequencies = abs(start - stop)
-    time_per_freq = total_time / total_frequencies / 3
-    
-    # ascend or descend based on order of frequencies passed
+    millis_per_freq = total_milliseconds / total_frequencies / 4
+
+    # descend
+    if start > stop:
+        for frequency in range(start, stop - 1, -1):
+            frequencies = _get_random_values(frequency)
+            for f in frequencies:
+                _generate_frequency(f, millis_per_freq)
     
     # ascend
     if start < stop:
-        for i in range(start, stop):
+        for frequency in range(start, stop + 1):
+            frequencies = _get_random_values(frequency, order='ascend')
+            for f in frequencies:
+                _generate_frequency(f, millis_per_freq)
             
-            # generate the decimal values for this freq
-            a, b, c = get_random_decimals(i, 'ascend')
-            freq_1 = i + a
-            freq_2 = i + b
-            freq_3 = i = c
-            
-            
-    
-signal = Pin(15, Pin.OUT)
 
-duty_cycle = get_duty_cycle(0.15)
-
-
+# this will generate 4 times the amount of frequencies that 
+# are passed in and they will be random values with each run
+run_frequencies(21, 1, 300)
         
